14317e9c32291eedf4412321d9604de9c6adf7dd
kidchang93 <lck930721@gmail.com>
2024-02-22 10:35:59 +0900

    Reducer 사용법

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Counter from "./Counter";
 import Info from "./Info";
+import CounterReducer from "./CounterReducer";
 
 const App = () => {
 	return (
 		<div>
 			<Info />
 			<Counter />
+			<hr />
+			<CounterReducer />
 		</div>
 	);
 };
--- src/Counter.js
+++ src/Counter.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Counter = () => {
 	const [count, setCount] = useState(0);
+
 	return (
 		<div>
 			<p>count: {count}</p>
